f175ee195ccd4345fbc7e6676fa0ee8404cb73f3
kuriyosh <yosyos0306@gmail.com>
2024-01-08 23:07:30 +0900

    17, 18

---
+++ src/17.rs
@@ -0,0 +1,20 @@
+use polars::lazy::prelude::*;
+use std::env::current_dir;
+use std::path::Path;
+
+fn main() {
+    let data_dir_path = Path::new(&current_dir().unwrap()).join("data");
+    let df = LazyCsvReader::new(data_dir_path.join("customer.csv"))
+        .has_header(true)
+        .finish()
+        .unwrap();
+
+    let result = df
+        .select([col("*")])
+        .sort("birth_day", Default::default())
+        .collect()
+        .unwrap()
+        .head(Some(10));
+
+    println!("{:?}", result);
+}
